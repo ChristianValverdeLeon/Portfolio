36c4ed356e45e266d96da7d18a60aca461942701
unknown <christianvaleon@gmail.com>
2025-02-16 03:37:10 +0100

    2 versiones de correo

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cobomaster56@gmail.com'  # Reemplazar con tu correo
-app.config['MAIL_PASSWORD'] = 'Cobo.password'  # Reemplazar con tu contraseña
+app.config['MAIL_PASSWORD'] = 'mxpw soko nevz kmzh'  # Reemplazar con tu contraseña
 app.config['MAIL_DEFAULT_SENDER'] = 'cobomaster56@gmail.com'  # Reemplazar con tu correo
 
 mail = Mail(app)  # Crear una instancia de Flask-Mail
